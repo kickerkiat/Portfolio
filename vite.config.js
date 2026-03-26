9eb6ec3ed549c0ec74f6ecda4ef68beed61f1094
kickerkiat <jakeliankhaikiat@gmail.com>
2026-03-27 03:03:27 +0800

    repo update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: 'https://kickerkiat.github.io/Portfolio/', // ⚠️ IMPORTANT
+    base: 'https://github.com/kickerkiat/Portfolio/', // ⚠️ IMPORTANT
 });
